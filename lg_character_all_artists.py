4d916f295f5ed95d0c5fa0945d92a84495be4686
Harish Thuwal <hthuwal@gmail.com>
2018-04-25 13:08:09 +0530

    Add epoch count of previous cycle as bias

--- lg_character_all_artists.py
+++ lg_character_all_artists.py
@@ -241,7 +241,7 @@ trainset1 = LyricsGenerationDataset(csv_file_path='songdata.csv')  # dataset of 
 trainset2 = FabolousDataset(min_num_words=175)  # fabulous dataset
 
 
-def train(model_file, trainset, out_folder, batch_size=1, epochs=100):
+def train(model_file, trainset, out_folder, batch_size=1, epochs=100, bias=0):
     trainset_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=False, num_workers=4, drop_last=True)
     rnn = LG_LSTM(input_size=len(all_characters) + 1, hidden_size=128, num_classes=len(all_characters))
     if os.path.exists(model_file):
@@ -281,14 +281,14 @@ def train(model_file, trainset, out_folder, batch_size=1, epochs=100):
 
             optimizer.step()
 
-        print("Epoch %d/%d: Total epochs:%d Loss:%f" % (epoch_number, epochs, rnn.epochs, loss))
+        print("Epoch %d/%d: Total epochs:%d Loss:%f" % (epoch_number, epochs, epoch_number + bias, loss))
         print("Saving Model %s" % (model_file))
         torch.save(rnn.state_dict(), model_file)
 
         if rnn.epochs % 10 == 0:
             sent = sample_from_rnn(rnn)
             print("Generated\n", sent)
-            out_f = open('%s/%d.txt' % (out_folder, rnn.epochs), 'w')
+            out_f = open('%s/%d.txt' % (out_folder, epoch_number + bias), 'w')
             out_f.write("%f\n" % (loss) + sent)
             out_f.close()
 
@@ -296,6 +296,6 @@ def train(model_file, trainset, out_folder, batch_size=1, epochs=100):
 
 
 if sys.argv[1] == "fab":
-    train("lg_char_fabolous.model", trainset2, "fabolous_out", batch_size=1, epochs=20000)
+    train("lg_char_fabolous.model", trainset2, "fabolous_out", batch_size=1, epochs=500, bias=500)
 else:
     train("lg_char_kaggle.model", trainset1, "lg", batch_size=100, epochs=100)
